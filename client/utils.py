27a2cbd0fbb4867a9646f311124a9494dfc04442
Quentin <marquesquentin@icloud.com>
2024-12-14 20:11:31 +0200

    add rc4

--- client/utils.py
+++ client/utils.py
@@ -1,10 +1,3 @@
-def xor_encrypt_decrypt(text, key):
-    key = str(key)
-    encrypted_text = ""
-    for i in range(len(text)):
-        encrypted_text += chr(ord(text[i]) ^ ord(key[i % len(key)]))
-    return encrypted_text
-
 def read_config(file_path):
     config = {}
     try:
@@ -17,3 +10,39 @@ def read_config(file_path):
         print("Configuration file not found. Please create 'wifi_config.txt'.")
     return config
 
+def xor_encrypt_decrypt(text, key):
+    key = str(key)
+    encrypted_text = ""
+    for i in range(len(text)):
+        encrypted_text += chr(ord(text[i]) ^ ord(key[i % len(key)]))
+    return encrypted_text
+
+def ksa(key,S):
+    key = [ord(char) for char in key] # string to byte
+    j = 0
+    for i in range(256):
+        j = (j + S[i] + key[i % len(key)]) % 256 # Now, j relies on the key
+        S[i], S[j] = S[j], S[i]  # Swap S[i] and S[j]
+    return S
+
+def prga(S, length):
+    i,j = 0,0
+    key_stream = []
+
+    for _ in range(length):
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+
+        key_stream_byte = S[(S[i] + S[j]) % 256]
+        key_stream.append(key_stream_byte)
+
+    return key_stream
+
+def rc4(text, key):
+    S = list(range(256))
+    S = ksa(str(key),S)
+    keystream = prga(S,16)
+    encrypted = xor_encrypt_decrypt(text,keystream)
+
+    return encrypted

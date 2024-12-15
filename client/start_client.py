9daf443f3a7b3d479e1183d3d5459ebff166e8cb
Quentin <marquesquentin@icloud.com>
2024-12-15 21:56:50 +0200

    remove MAC

--- client/start_client.py
+++ client/start_client.py
@@ -6,7 +6,7 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from utils.file import read_config, select_encryption
 
-required_parameters = ['IP', 'PORT', 'MAC', 'PASSWORD','SECURITY']
+required_parameters = ['IP', 'PORT', 'PASSWORD','SECURITY']
 config = read_config("wifi_config.txt", required_parameters)
 security = config["SECURITY"]
 encryption = select_encryption[security]
@@ -29,14 +29,19 @@ def authentification():
 
     return client
 
+def send_mac(client):
+    MAC = config["MAC"]
+    client.send(MAC.encode())
+
 def start_client():
 
     client = authentification()
+    send_mac(client)
 
     while True:
-        message = input("message ? \n")
+        data = input("Data to send to router : ")
 
-        encrypted_message = encryption(message, config["PASSWORD"])
+        encrypted_message = encryption(data, config["PASSWORD"])
         client.send(encrypted_message.encode())
 
         encrypted_response = client.recv(1024).decode()
